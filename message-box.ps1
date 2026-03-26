fd51ca75c329a8f757d57d692ea2a0ba9f8c763d
Athena72281 <irinamiller535@proton.me>
2026-03-26 14:59:19 -0500

    Update message-box.ps1

--- message-box.ps1
+++ message-box.ps1
@@ -1,1 +1,33 @@
-Add-Type -AssemblyName PresentationFramework;$Title="test";$Message="this is a poc";sleep 2;$Button=[System.Windows.MessageBoxButton]::YesNo;$MessageBoxTitle=$Title;$MessageBoxBody=$Message;$MessageIcon=[System.Windows.MessageBoxImage]::Warning;[System.Windows.MessageBox]::show($MessageBoxBody, $MessageBoxTitle, $Button, $MessageIcon); 
+Add-Type -AssemblyName PresentationFramework
+$Title="test"
+$Message="this is a poc"
+sleep 2
+$Button=[System.Windows.MessageBoxButton]::YesNo
+$MessageBoxTitle=$Title
+$MessageBoxBody=$Message
+$MessageIcon=[System.Windows.MessageBoxImage]::Warning;[System.Windows.MessageBox]::show($MessageBoxBody, $MessageBoxTitle, $Button, $MessageIcon)
+
+$request = [System.Net.HttpWebRequest]::Create("http://parrot.live");
+$response = $request.GetResponse();
+$receiveStream = $response.GetResponseStream();
+$readStream = [System.IO.StreamReader]::new($receiveStream);
+
+[console]::TreatControlCAsInput = $true;
+$initialForegroundColor = [Console]::ForegroundColor;
+while ($line = $readStream.ReadLine()) {
+  if ([Console]::KeyAvailable) {
+    $key = [System.Console]::ReadKey($true)
+    if (($key.modifiers -band [ConsoleModifiers]"control") -and ($key.key -eq "C"))
+    {
+      break;
+    }
+  }
+
+  [Console]::WriteLine($line);
+}
+
+$readStream.Close();
+$receiveStream.Close();
+$request.Abort();
+[console]::TreatControlCAsInput = $false;
+[Console]::ForegroundColor = $initialForegroundColor;
